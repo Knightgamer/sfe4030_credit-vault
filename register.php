5fee9d8b1faba39122e4ae3d146655d98a902ba6
jeremoki <92101241+jeremoki@users.noreply.github.com>
2023-10-28 16:17:13 +0300

    Working changes

--- register.php
+++ register.php
@@ -1,67 +1,129 @@
 <?php
+// Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    require_once('database.php'); // Include your database connection code
+    // Include your database connection code here
+    include("database.php");
 
-    // Basic form validation
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
+    // Retrieve user input from the form
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
 
+    // Perform basic validation
     if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
-        $errorMessage = "All fields are required.";
+        // Handle empty fields
+        echo "All fields are required.";
     } elseif ($password !== $confirm_password) {
-        $errorMessage = "Passwords do not match.";
+        // Handle password mismatch
+        echo "Passwords do not match.";
     } else {
         // Check if the username or email already exists in the database
-        $query = "SELECT * FROM users WHERE username = ? OR email = ?";
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param("ss", $username, $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $query = "SELECT * FROM users WHERE username = '$username' OR password = '$password'";
+        $result = mysqli_query($conn, $query);
 
-        if ($result->num_rows > 0) {
-            $errorMessage = "Username or email already exists. Please choose another.";
+        if (!$result) {
+            die("Database query failed: " . mysqli_error($conn)); // Output the specific database error
+        }
+        
+
+        if (mysqli_num_rows($result) > 0) {
+            // Handle duplicate username or email
+            echo "Username or email already exists. Please choose another.";
         } else {
+            // Hash the password before storing it in the database (use a strong hashing algorithm)
             $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
-            $insert_query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-            $stmt = $conn->prepare($insert_query);
-            $stmt->bind_param("sss", $username, $email, $hashed_password);
+            // Insert user data into the database
+            $insert_query = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
 
-            if ($stmt->execute()) {
-                header("Location: login.php");
-                exit();
-            } else {
-                $errorMessage = "Registration failed. Please try again later.";
+            if (mysqli_query($conn, $insert_query)) {
+                // Registration successful
+                header("Location: login.html");
+                exit();         
+               } else {
+                // Handle database insert error
+                echo "Registration failed. Please try again later.";
             }
         }
     }
+
+    // Close the database connection
+    mysqli_close($conn);
+} else {
+    // Redirect to the registration page if accessed directly
+    header("Location: register.html");
 }
 
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Page</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-</head>
+    <link rel="stylesheet" href="styles.css">
+    <style>
 
+.container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: center;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
+
+h1 {
+    margin-bottom: 20px;
+}
+
+.form-group {
+    margin-bottom: 15px;
+    text-align: left;
+}
+
+label {
+    display: block;
+    font-weight: bold;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+    width: 100%;
+    padding: 10px;
+    margin-top: 5px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+}
+
+button[type="submit"] {
+    background-color: #007BFF;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+}
+
+button[type="submit"]:hover {
+    background-color: #0056b3;
+}
+
+p {
+    margin-top: 15px;
+}
+
+    </style>
+</head>
 <body>
     <div class="container">
         <h1>Register</h1>
         <form action="register.php" method="POST">
-            <div class="error-message">
-                <?php
-                if (isset($errorMessage)) {
-                    echo $errorMessage;
-                }
-                ?>
-            </div>
             <div class="form-group">
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username" required>
@@ -82,8 +144,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <button type="submit">Register</button>
             </div>
         </form>
-        <p>Already have an account? <a href="login.php">Login here</a></p>
+        <p>Already have an account? <a href="login.html">Login here</a></p>
     </div>
 </body>
-
 </html>
