bccd44287ee1c2f2de6ebf128e1f649fc506880d
iWB-Git <sbp1784@gmail.com>
2023-10-28 21:40:29 +0300

    added j-query datatables to the existing view_client page

--- view_clients.php
+++ view_clients.php
@@ -1,230 +1,127 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>View Clients</title>
-    <!--<link rel="stylesheet" type="text/css" href="styles.css"> -->
-    
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            // Hide the success message after 3 seconds
-            setTimeout(function() {
-                $('.success-message').fadeOut('slow');
-            }, 3000); // 3000 milliseconds = 3 seconds
-        });
-    </script>
+    <link rel="stylesheet" type="text/css" href="./assets/css/view_client.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 
-    <style>
-        /* styles.css */
-body {
-    font-family: Arial, sans-serif;
-    
-}
-/* Set background image */
-body {
-        background-image: url('background.png'); /* Replace 'background.png' with the path to your image */
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-
-    /* Top navigation bar */
-    .navbar {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color: #333;
-        padding: 10px 20px;
-    }
-
-    .navbar-logo {
-        display: flex;
-        align-items: center;
-        color: #fff;
-        text-decoration: none;
-    }
-
-    .navbar-logo img {
-        max-width: 100px;
-        margin-right: 10px;
-    }
-
-    .navigation {
-        text-align: center;
-    }
-    /* Style for navigation buttons */
-    .nav-button, button {
-        display: inline-block;
-        padding: 10px 20px;
-        width: 150px; /* Set a fixed width for all buttons */
-        background-color: #007bff;
-        color: #fff;
-        text-decoration: none;
-        margin: 0 10px;
-        border-radius: 5px;
-        transition: background-color 0.3s ease;
-        cursor: pointer;
-        border: none; /* Remove button border */
-        outline: none; /* Remove button outline */
-    }
-    .nav-button:hover, button:hover {
-        background-color: #0056b3;
-    }
-
-    h1 {
-        text-align: center;
-    }
-
-    table.excel-sheet {
-        width: 400px;
-        border-collapse: collapse;
-        margin-top: 20px;
-        margin: 0 auto;
-    }
-
-    .header-row {
-        background-color: #f2f2f2;
-        font-weight: bold;
-    }
-
-    .header-row th {
-        padding: 50px;
-        text-align: left;
-        border-bottom: 2px solid #ddd;
-    }
-    .main {
-        text-align: center; /* Center align the content within .main */
-        margin-top: 50px;
-        
-    }
-
-    .excel-cell {
-        padding: 10px;
-        border-bottom: 1px solid #ddd;
-        vertical-align: top; /* Align content to the top of the cell */
-    }
-
-    .excel-row:nth-child(even) {
-        background-color: #f2f2f2;
-    }
-    .excel-cell.single-line {
-        white-space: nowrap;
-        overflow: hidden; /* Hide any overflow */
-        text-overflow: ellipsis;
-    }
-
-
-    .excel-row td {
-        padding: 10px;
-        border-bottom: 1px solid #ddd;
-    }
-
-    .success-message {
-        color: green;
-        font-weight: bold;
-        margin-top: 10px;
-    }
-
-    .container {
-        max-width: 900px;
-        margin: 0 auto;
-        padding: 20px;
-
-        
-    }
-
-    </style>
-
+    <!-- DataTables CSS and extensions -->
+    <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css" rel="stylesheet">
 
 </head>
+
 <body>
     <div class="navbar">
         <a href="#" class="navbar-logo">
             <img src="images/Volks Elevator Logo.png" alt="Logo">
-            
         </a>
         <div class="navigation">
             <a href="index.php" class="nav-button">Homepage</a>
             <a href="add_client.html" class="nav-button">Add New Client</a>
-            <a href="generate_report.html" class="nav-button">Generate Report</a>
+            <!-- <a href="generate_report.html" class="nav-button">Generate Report</a> -->
         </div>
     </div>
-   
+
     <div class="main">
-    <h1>View Clients</h1>
+        <h1>View Clients</h1>
+        <div class="table-container">
+            <?php
+            include 'database.php';
 
-<?php
-include 'database.php';
+            $sql = "SELECT * FROM clients";
+            $result = $conn->query($sql);
+            session_start();
 
-$sql = "SELECT * FROM clients";
-$result = $conn->query($sql);
-session_start();
-if (isset($_SESSION['new_client_added']) && $_SESSION['new_client_added'] === true) {
-    echo '<p class="success-message">Client successfully added!</p>';
-    unset($_SESSION['new_client_added']); // Clear the session variable
-}
-
-
-if ($result->num_rows > 0) {
-    echo '<table id="example" class="excel-sheet">';
-    echo '<tr class="header-row">';
-    echo '<th>Client Name</th>';
-    echo '<th>Location</th>';
-    echo '<th>Building Name</th>';
-    echo '<th>No_units</th>';
-    echo '<th>Amount Payable</th>';
-    echo '<th>Advance</th>'; 
-    echo '<th>Total Amount Paid</th>';
-    echo '<th>Remaining Value</th>';
-    echo '<th>Payment Status</th>';
-    echo '<th>Action</th>';
-
-    echo '</tr>';
-
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr class="excel-row">';
-        echo '<td class="excel-cell">' . $row['client_name'] . '</td>';
-        echo '<td class="excel-cell">' . $row['location'] . '</td>'; 
-        echo '<td class="excel-cell">' . $row['building_name'] . '</td>'; 
-        echo '<td class="excel-cell">' . $row['No_units'] . '</td>';
-        echo '<td class="excel-cell">' . $row['amount_payable'] . '</td>';
-        echo '<td class="excel-cell">' . $row['Advance'] . '</td>';
-        echo '<td class="excel-cell">' . $row['total_amount_paid'] . '</td>';
-        echo '<td class="excel-cell">' . $row['remainingValue'] . '</td>';
-        echo '<td class="excel-cell">' . $row['payment_status'] . '</td>';
-        echo '<td class="excel-cell"> <a class="edit-button" href="edit.php?id=' . $row['id'] . '">Edit</button></a>
-        <br> <br><button class="delete-button" data-client-id="' . $row['id'] . '">Delete</button></td>';
-        echo '</tr>';
-
-        /*echo '<td class="excel-cell"> <button a class="edit-button" href="edit.php?id=' . $row['id'] . '">Edit</button></a><td>';
-        echo '</tr>';
-        echo '<td class="excel-cell"><button class="delete-button" data-client-id="' . $row['id'] . '">Delete</button></td>';
-        echo '</tr>';*/
-    }   
-
-    echo '</table>';
-} else {
-    echo 'No clients found.';
-}
-
-
-$conn->close();
-?>
-
-
-    </div>
-    <script>
-        // Automatically hide the success message after 3 seconds
-        setTimeout(function() {
-            var successMessage = document.getElementById('successMessage');
-            if (successMessage) {
-                successMessage.style.display = 'none';
+            if (isset($_SESSION['new_client_added']) && $_SESSION['new_client_added'] === true) {
+                echo '<p class="success-message">Client successfully added!</p>';
+                unset($_SESSION['new_client_added']); // Clear the session variable
             }
-        }, 3000); // 3000 milliseconds = 3 seconds
 
-        $(document).on('click', '.delete-button', function() {
-        var clientId = $(this).data('client-id'); // Corrected to 'client-id'
+            if ($result->num_rows > 0) {
+                $tableRows = '';
+                while ($row = $result->fetch_assoc()) {
+                    $tableRows .= <<<ROW
+            <tr class="excel-row">
+                <td class="excel-cell">{$row['client_name']}</td>
+                <td class="excel-cell">{$row['location']}</td>
+                <td class="excel-cell">{$row['building_name']}</td>
+                <td class="excel-cell">{$row['No_units']}</td>
+                <td class="excel-cell">{$row['amount_payable']}</td>
+                <td class="excel-cell">{$row['Advance']}</td>
+                <td class="excel-cell">{$row['total_amount_paid']}</td>
+                <td class="excel-cell">{$row['remainingValue']}</td>
+                <td class="excel-cell">{$row['payment_status']}</td>
+                <td class="excel-cell action-cell">
+    <a class="action-button edit-button" href="edit.php?id={$row['id']}"><i class="fas fa-edit"></i></a>
+    <a class="action-button delete-button" data-client-id="{$row['id']}"><i class="fas fa-trash-alt"></i></a>
+</td>
+
+            </tr>
+ROW;
+                }
+            } else {
+                $noClientsMessage = "No clients found.";
+            }
+
+            $conn->close();
+            ?>
+
+            <?php if (isset($tableRows)): ?>
+                <table id="example" class="excel-sheet">
+                    <thead>
+                        <tr class="header-row">
+                            <th>Client Name</th>
+                            <th>Location</th>
+                            <th>Building Name</th>
+                            <th>No_units</th>
+                            <th>Amount Payable</th>
+                            <th>Advance</th>
+                            <th>Total Amount Paid</th>
+                            <th>Remaining Value</th>
+                            <th>Payment Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?= $tableRows; ?>
+                    </tbody>
+                </table>
+            <?php elseif (isset($noClientsMessage)): ?>
+                <p>
+                    <?= $noClientsMessage; ?>
+                </p>
+            <?php endif; ?>
+
+        </div>
+    </div>
+</body>
+
+
+<!-- jQuery library -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- DataTables JS and extensions -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.3.0/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+
+<script>
+    $(document).ready(function () {
+        // Hide the success message after 3 seconds
+        setTimeout(function () {
+            $('.success-message').fadeOut('slow');
+        }, 3000); // 3000 milliseconds = 3 seconds
+    });
+
+    $(document).on('click', '.delete-button', function () {
+        var clientId = $(this).data('client-id');
         var confirmDelete = confirm('Are you sure you want to delete this client?');
 
         if (confirmDelete) {
@@ -232,22 +129,30 @@ $conn->close();
             $.ajax({
                 url: 'delete.php',
                 method: 'POST',
-                data: { client_id: clientId }, // Change to 'client_id'
-                success: function(response) {
+                data: {
+                    client_id: clientId
+                },
+                success: function (response) {
                     // Handle success, e.g., show the response in an alert or update the page
                     alert(response); // Display the success or error message
                     window.location.reload(); // Refresh the page after deletion
                 },
-                error: function() {
+                error: function () {
                     alert('An error occurred while deleting the client.');
                 }
             });
         }
     });
+</script>
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+        });
+    });
+</script>
 
-
-
-    </script>
-    
-    </body>
 </html>
